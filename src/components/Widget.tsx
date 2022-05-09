dfaa7a5f30d5c2c1d34b879a80840ee68e81f048
didiegopds2684 <diegoxdavi@gmail.com>
2022-05-09 15:40:40 -0300

    add buttons and assets

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,11 +1,12 @@
 import {ChatTeardropDots} from "phosphor-react";
 import {Popover} from '@headlessui/react';
+import {WidgetForm} from "./WidgetForm";
 
 export function Widget() {
     return (
-        <Popover className="absolute bottom-5 right-5">
-            <Popover.Panel>Hello world</Popover.Panel>
-            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group ">
+        <Popover className="absolute bottom-4 right-4 flex flex-col items-end md:bottom-10 md:right-10">
+            <Popover.Panel>{WidgetForm}</Popover.Panel>
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
                 <ChatTeardropDots className="w-6 h-6" size="200" weight="bold"/>
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
                     <span className="pl-2"/>
